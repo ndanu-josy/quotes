d3e917a499cdc1d389b669b0563f8d0171fdf965
josphine-Ndanu <josephine.nguli@student.moringaschool.com>
2021-05-15 15:18:06 +0300

    create new quote object

--- src/app/iqoutes-form/iqoutes-form.component.ts
+++ src/app/iqoutes-form/iqoutes-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Quote } from '../quotes';
 
 @Component({
   selector: 'app-iqoutes-form',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./iqoutes-form.component.css']
 })
 export class IqoutesFormComponent implements OnInit {
+
+  newQuote = new Quote(0, '','','', new Date(), 0, 0);
+  @Output() addQuote = new EventEmitter<Quote>()
+
+  submitQuote(){
+    this.addQuote.emit(this.newQuote);
+    this.newQuote = new Quote(0, '', '', '', '', new Date(), 0, 0);
+  }
 
   constructor() { }
 
